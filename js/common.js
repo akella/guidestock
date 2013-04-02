33118b84253abc6d3d0ff8cd1630ce1bbbba259d
akella <akella.a@gmail.com>
2013-04-02 13:23:03 +0300

    placeholder fix

--- js/common.js
+++ js/common.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
 
 //Placeholder
-var placeholder_val;
-$('.input').focusin(function(){
-    placeholder_val = $(this).val();
-    $(this).addClass('focus');
-});
-// remove placeholder from the current input
-$('.input').focusout(function(){
-    $(this).val(placeholder_val);
-    $(this).removeClass('focus');
-});
+// var placeholder_val;
+// $('.input').focusin(function(){
+//     placeholder_val = $(this).val();
+//     $(this).addClass('focus');
+// });
+// // remove placeholder from the current input
+// $('.input').focusout(function(){
+//     $(this).val(placeholder_val);
+//     $(this).removeClass('focus');
+// });
+
+$("[placeholder]").togglePlaceholder();
+
 
 });
+
+$.fn.togglePlaceholder = function() {
+    return this.each(function() {
+        $(this)
+        .data("holder", $(this).attr("placeholder"))
+        .focusin(function(){
+            $(this).attr('placeholder','');
+        })
+        .focusout(function(){
+            $(this).attr('placeholder',$(this).data('holder'));
+        });
+    });
+};
