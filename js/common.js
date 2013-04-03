45798b60346be9a069dd8867fae15c75f4066b51
akella <akella.a@gmail.com>
2013-04-03 19:03:53 +0300

    js merge

--- js/common.js
+++ js/common.js
@@ -1,26 +1,8 @@
 $(document).ready(function() {
 
-<<<<<<< HEAD
-//Placeholder
-// var placeholder_val;
-// $('.input').focusin(function(){
-//     placeholder_val = $(this).val();
-//     $(this).addClass('focus');
-// });
-// // remove placeholder from the current input
-// $('.input').focusout(function(){
-//     $(this).val(placeholder_val);
-//     $(this).removeClass('focus');
-// });
-
 $("[placeholder]").togglePlaceholder();
 
 
-=======
-//placeholder
-$("[placeholder]").togglePlaceholder();
-
->>>>>>> fixes
 });
 
 $.fn.togglePlaceholder = function() {
@@ -29,17 +11,11 @@ $.fn.togglePlaceholder = function() {
         .data("holder", $(this).attr("placeholder"))
         .focusin(function(){
             $(this).attr('placeholder','');
-<<<<<<< HEAD
-        })
-        .focusout(function(){
-            $(this).attr('placeholder',$(this).data('holder'));
-=======
             $(this).addClass('focus');
         })
         .focusout(function(){
             $(this).attr('placeholder',$(this).data('holder'));
             $(this).removeClass('focus');
->>>>>>> fixes
         });
     });
 };
