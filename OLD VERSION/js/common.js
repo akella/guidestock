06320910a57be11ede52056fe0b96b3a31382344
Artur Moroz <morozarturv@gmail.com>
2013-05-27 10:16:06 +0300

    first done! (redesign)

---
+++ OLD VERSION/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+$("[placeholder]").togglePlaceholder();
+
+
+});
+
+$.fn.togglePlaceholder = function() {
+    return this.each(function() {
+        $(this)
+        .data('holder', $(this).attr('placeholder'))
+        .focusin(function(){
+            $(this).attr('placeholder','');
+            $(this).addClass('focus');
+        })
+        .focusout(function(){
+            $(this).attr('placeholder',$(this).data('holder'));
+        });
+    });
+};
